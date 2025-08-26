732fd63eda09dec0a014e4835e482d918ddb174b
MikeGPQ <miguelgarciaparra@hotmail.com>
2025-08-25 21:22:23 -0600

    25/08/2025

--- dataManager.py
+++ dataManager.py
@@ -1,14 +1,26 @@
 import faker
 import polars
 import matplotlib.pyplot as plt
+import os
 
 gen = faker.Faker()
 
 class dataManager:
     def __init__(self):
-        self.dataFrame = polars.DataFrame()
+        self.dataFrame = polars.DataFrame().lazy()
+        self.copy = self.dataFrame
 
-    def genRandData(self,units=5):
+    def setDataFrame(self,dataFile):
+        match os.path.splitext(dataFile.filename)[1]:
+            case ".csv":
+                self.dataFrame = polars.read_csv(dataFile).lazy()
+            case ".xlsx":
+                self.dataFrame = polars.read_excel(dataFile.read()).lazy()
+            case ".json":
+                self.dataFrame = polars.read_json(dataFile).lazy()
+        self.copy = self.dataFrame.clone()
+
+    def genRandData(self,units):
         data = []
         for _ in range(units):
             firstName = gen.first_name()
@@ -18,35 +30,71 @@ class dataManager:
                 "First Name": firstName,
                 "Last Name": lastName,
                 "Email": email,
+                "Country": gen.country(),
                 "Phone": gen.phone_number(),
                 "Salary": gen.pyint(min_value=8, max_value=10000)
             }
             data.append(user)
-        self.dataFrame = polars.DataFrame(data)
+        self.dataFrame = polars.DataFrame(data).lazy()
+        self.copy = self.dataFrame.clone()
     
     def getHTML(self):
-        if self.dataFrame.is_empty():
+        if self.dataFrame.limit(1).collect().is_empty():
             return None
-        return self.dataFrame._repr_html_()
+        
+        return self.dataFrame.collect()._repr_html_()
     
     def getColumns(self):
-        if self.dataFrame.is_empty():
+        if self.dataFrame.limit(1).collect().is_empty():
             return None
         return self.dataFrame.columns
     
-    def getGraph(self,Xaxis,Yaxis,graphType):
-        if self.dataFrame.is_empty():
+    def getGraph(self,Xaxis,Yaxis,graphType,format):
+        if self.dataFrame.limit(1).collect().is_empty():
             return None
-        fig, ax = plt.subplots(figsize=(10,6))
+        nRows = self.dataFrame.select(polars.count()).collect().item()
+
+        width = 15
+        height = 10
+        xFactor = 1 + (nRows/500)
+        yFactor = 1 + (nRows/1000)
+        height = min(max(height*yFactor,10),50)
+        width = min(max(width*xFactor,15),50)
+
+        fig, ax = plt.subplots(figsize=(width, height))
+
         plt.style.use("fivethirtyeight")
+        graphDF = self.dataFrame.select(polars.col(Xaxis),polars.col(Yaxis)).collect()
         if graphType == "lineGraph":
-            ax.plot(self.dataFrame[Xaxis],self.dataFrame[Yaxis])
+            ax.plot(graphDF[Xaxis],graphDF[Yaxis])
         else:
-            ax.bar(self.dataFrame[Xaxis],self.dataFrame[Yaxis])
+            ax.bar(graphDF[Xaxis],graphDF[Yaxis])
+
         ax.set_title("Graph Data Frame")
         ax.set_xlabel(Xaxis)
         ax.set_ylabel(Yaxis)
+        if nRows > 20:
+            plt.xticks(rotation=45, ha='right')
         plt.tight_layout()
-        fig.savefig("static/images/graph.png")
-        return "static/images/graph.png"
+
+        if format == "PNG":
+            fig.savefig("static/images/graph.png")
+            return "static/images/graph.png"
+        else:
+            fig.savefig("static/images/graph.pdf")
+            return "static/images/graph.pdf"
         
+    def getFilterData(self,filterCol,filterVal):
+        col_type = self.dataFrame.schema[filterCol]
+        if col_type == "foo":
+            if polars.Int64 in (polars.Int8, polars.Int16, polars.Int32, polars.Int64):
+                filterVal = int(filterVal)
+            elif polars.Float64 in (polars.Float32, polars.Float64):
+                filterVal = float(filterVal)
+
+        self.dataFrame = self.dataFrame.filter(polars.col(filterCol)==filterVal)
+    
+    def getSortData(self,sortColumn,descendingValue):
+        self.dataFrame = self.dataFrame.sort(by=sortColumn,descending=descendingValue)
+        
+    
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from dataManager import dataManager
 
 
@@ -13,15 +13,34 @@ def load():
     if request.method == "POST":
         match request.form["action"]:
             case "Generate random data frame":
-                dm.genRandData()
-            case "Graph Data":
+                units = int(request.form.get("units4DataFrame"))
+                dm.genRandData(units)
+            case "Export":
                 Xaxis = request.form.get("Xaxis")
                 Yaxis = request.form.get("Yaxis")
                 graphType = request.form.get("graphType")
-                graph = dm.getGraph(Xaxis, Yaxis, graphType)
+                format = request.form.get("graphFormat")
+                graph = dm.getGraph(Xaxis, Yaxis, graphType,format)
+                if format == "PNG":
+                    return send_file(graph,as_attachment=True,download_name="graph.png")
+                else:
+                    return send_file(graph,as_attachment=True,download_name="graph.pdf")
+            case "Upload":
+                dm.setDataFrame(request.files["dataFile"])
+            case "Filter":
+                filertCol = request.form.get("filterColumn")
+                filterVal = request.form.get("filterValue")
+                dm.getFilterData(filertCol,filterVal)
+            case "Restore":
+                dm.dataFrame = dm.copy
+            case "Sort":
+                sortColumn = request.form.get("sortColumn")
+                descendingValue = eval(request.form.get("descendingValue"))
+                dm.getSortData(sortColumn,descendingValue)
+
     
         
-    return render_template("home.html",dataFrame = dm.getHTML(),columns = dm.getColumns(),graph = graph)
+    return render_template("home.html",dataFrame = dm.getHTML(),columns = dm.getColumns())
 
 if __name__== "__main__":
     app.run(debug=True)
